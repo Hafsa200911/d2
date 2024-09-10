539733822b26f84771d1aa900ae5f3b05bae7994
Hafsa <hafsmoha2009@gmail.com>
2024-09-10 17:45:25 +0300

    first commit

---
+++ d2.js
@@ -0,0 +1,23 @@
+
+var x=prompt("Enter your Degree:");
+
+
+if(x>90 || x==90)
+{
+    console.log("Excellent :)");
+}
+
+else if(x>70 || x==70 )
+{
+    console.log("Good :|");
+}
+
+else if(x>50 || x==50)
+{
+    console.log("Bad :(");
+}
+
+else
+{
+    console.log("Very bad , try again :((((");
+}
